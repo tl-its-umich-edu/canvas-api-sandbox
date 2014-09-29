c9aed0365e5c2548f87328844ff2c6cd41eb4959
David Haines <dlhaines@umich.edu>
2014-09-29 16:06:58 -0400

    refactor ruby code to reusable methods

---
+++ ruby-rest/request_utils.rb
@@ -0,0 +1,34 @@
+### Utilities to run requests against a REST api
+
+def setup_logger 
+  log = Logger.new(STDOUT)
+  log.level = Logger::DEBUG
+  RestClient.log = log
+end
+
+def print_response (response)
+  puts response.body
+  json_data = response.body
+  parsed = JSON.parse(json_data)
+  print "#{parsed}"
+  print "\n";
+end
+
+def run_request(request)
+#  request="Terms"
+  url="#{HOST}/#{request}"
+
+  response = RestClient.get url, { :Authorization =>"Bearer #{TOKEN}",
+                                   :accept => :json,
+                                   :verify_ssl => OpenSSL::SSL::VERIFY_NONE}
+  return response
+end
+
+def do_request(request) 
+  setup_logger
+
+  response = run_request request
+
+  print_response response
+end
+
--- ruby-rest/umScheduleOfClasses.rb
+++ ruby-rest/umScheduleOfClasses.rb
@@ -1,33 +1,12 @@
 #!/usr/bin/env ruby
 
-# List terms for UM 
-
 require "json"
 require "rest-client"
 require "logger"
 
 load "user_info.rb"
+load "request_utils.rb"
 
-REQUEST="Terms"
-
-# Web Service call
-
-URL="#{HOST}/#{REQUEST}"
-
-log = Logger.new(STDOUT)
-log.level = Logger::DEBUG
-RestClient.log = log
-
-response = RestClient.get URL, { :Authorization =>"Bearer #{TOKEN}",
-                                 :accept => :json,
-                                 :verify_ssl => OpenSSL::SSL::VERIFY_NONE}
-puts response.body
-json_data = response.body
-
-parsed = JSON.parse(json_data)
-
-print "#{parsed}"
-
-print "\n";
+do_request("Terms")
 #end
 
